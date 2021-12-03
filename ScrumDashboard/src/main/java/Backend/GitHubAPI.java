e57ab4784c7bdfda19a8db035a4a63dc8eda9413
Bernardo-Bento <bmlcb@iscte-iul.pt>
2021-12-03 17:09:40 +0000

    updated GitHubAPI and pom

--- ScrumDashboard/src/main/java/Backend/GitHubAPI.java
+++ ScrumDashboard/src/main/java/Backend/GitHubAPI.java
@@ -1,5 +1,41 @@
 package Backend;
 
-public class GitHubAPI {
+import java.awt.EventQueue;
+import java.io.IOException;
+import java.util.Base64;
 
-}
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+import org.kohsuke.github.GitHubBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import Frontend.GUI;
+
+
+
+public class GitHubAPI {
+	private static final String authorization = "Bearer: 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";
+	private static final String baseUrl = "https://api.github.com/repos/henrique-efonseca/ES-LETI-1Sem-2021-Grupo3";
+	private static final ObjectMapper objectMapper = new ObjectMapper();
+
+	private static GitHub github;
+	
+
+	    
+	  
+
+public static void main(String[] args) throws IOException {
+	
+	github = new GitHubBuilder().withOAuthToken("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").build();
+	GHRepository repository =  github.getRepository("henrique-efonseca/ES-LETI-1Sem-2021-Grupo3");
+	System.out.println(repository.getFullName());
+	System.out.println("meita: ");
+	
+	
+	System.out.println(repository.getReadme().getEncodedContent());
+	
+	    	
+	    }
+	    }
