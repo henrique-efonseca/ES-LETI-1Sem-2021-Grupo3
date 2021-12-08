352247c7c379db1271520a2b4d14e70461434204
appca1 <appca1@iscte-iul.pt>
2021-12-08 01:05:23 +0000

    Javadoc: PieChart, Sprint, Tabela

--- ScrumDashboard/src/main/java/Frontend/Sprint.java
+++ ScrumDashboard/src/main/java/Frontend/Sprint.java
@@ -9,6 +9,12 @@ import com.julienvey.trello.domain.Action;
 import com.julienvey.trello.domain.Card;
 import com.julienvey.trello.domain.Member;
 
+/**
+ *Trello API 
+ *@author Ricardo Paulo
+ *@version 1.0
+ */
+
 public class Sprint {
 
 	private String name;
@@ -17,50 +23,85 @@ public class Sprint {
 	private List<Card> cards = new ArrayList<>();
 	private HashMap<String, Double> realizados = new HashMap<String, Double>();
 	private HashMap<String, Double> previstos = new HashMap<String, Double>();
-	
+	/**
+	 * Constructor of a Sprint
+	 * @param n
+	 * name of sprint
+	 */
 	public Sprint(String n) {
 		// TODO Auto-generated constructor stub
 		this.name=n;
 		this.text = this.name.toUpperCase()+"\n";
 	}
-
+/**
+ * returns the name of a sprint
+ * @return
+ */
 	public String getName() {
 		return name;
 	}
-
+/**
+ * returns the text of a sprint
+ * @return
+ */
 	public String getText() {
 		return text;
 	}
-
+/**
+ * add text to a sprint
+ * @param text
+ */
 	public void addText(String text) {
 		this.text += text + "\n";
 	}
 	
+	/**
+	 * return the start & end date of a sprint
+	 * @return
+	 */
 	public String getDate() {
 		return date.toString();
 	}
-
+    /**
+     * add the start & end date to a sprint
+     * @param start
+     */
 	public void setDate(String start) {
 		this.date = start;
 	}
-
+    /**
+     * returns the cards of a sprint 
+     * @return
+     */
 	public List<Card> getCards() {
 		return cards;
 	}
-
+    /**
+     * add a card to a sprint
+     * @param c
+     * card
+     */
 	public void addCard(Card c) {
 		cards.add(c);
 	}
 	
-	public void reserLists(List<Member> nomes){
+	/**
+	 * Reset lists
+	 * @param nomes
+	 */
+	public void resetLists(List<Member> nomes){
     	for(Member m: nomes){
     		realizados.put(m.getUsername(),0.0);
     		previstos.put(m.getUsername(),0.0);
     	}
 	}
+	/**
+	 * add spent and estimate time on a Sprint by a member
+	 * @param nomes
+	 */
 	
-	public void addTimes(List<Member> nomes){
-		reserLists(nomes);
+		public void addTimes(List<Member> nomes){
+		resetLists(nomes);
 		for(Card c : cards){
 		List<Action> actions = c.getActions();
 			for (Action a : actions){
@@ -80,11 +121,22 @@ public class Sprint {
 		}
 	return;
 	}
-	
+	/**
+	 * returns time spent by a member
+	 * @param m
+	 * member
+	 * @return
+	 */
 	public double getActualTime(Member m){
 		return realizados.get(m.getUsername());
 	}
 	
+	/**
+	 * returns estimated time of a member
+	 * @param m
+	 * member
+	 * @return
+	 */
 	public double getEstimatedTime(Member m){
 		return previstos.get(m.getUsername());
 	}
